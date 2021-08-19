7bdb3ea173b7ab4797f536e895629111a00b83c7
eaeschl <edsel.aeschlimann@live.com>
2021-08-19 22:53:29 +0000

    mensagem

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+# print("Hello world!")
+a = "Edsel"
+# print("Hola "+a+", como estás?...")
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/hello')
+def pagina_inicial():
+    return "Hola "+a+", como estás?..."
+
+@app.route('/test')
+def pagina_inicial():
+    return "Esta es una prueba... "
