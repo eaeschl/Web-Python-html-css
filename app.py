0e763798ec7419feefc940f476d76ef77ef22ccc
eaeschl <edsel.aeschlimann@live.com>
2021-08-19 23:22:50 +0000

    mensagem

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 # print("Hello world!")
 a = "Edsel"
 # print("Hola "+a+", como estás?...")
-from flask import Flask
+import sqlite3
+from flask import Flask, request, session, g, redirect, abort, render_template, flash
+
+# configuração
+DATABASE = "blog.db"
+SECRET_KEY = 'pudim'
+
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABSE'])
+
 @app.route('/hello')
 def pagina_inicial():
     return "Hola "+a+", como estás?..."
-
-@app.route('/test')
-def pagina_inicial():
-    return "Esta es una prueba... "
